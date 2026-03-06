4e44ef99442bbe9b19dcbbff254b9d65c12bed16
Jacob Zastrow <252672008+zastrowj@users.noreply.github.com>
2026-03-06 19:54:10 +0000

    add login routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get "/login", :controller => "sessions", :action => "new"
+  get "/logout", :controller => "sessions", :action => "destroy"
+  
   get("/", { :controller => "places", :action => "index" })
   resources "entries"
   resources "places"
