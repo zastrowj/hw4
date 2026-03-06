1a6898a143dd340a381681f153c1b27dcd49ec58
Jacob Zastrow <252672008+zastrowj@users.noreply.github.com>
2026-03-06 21:27:35 +0000

    add post for login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get "/login", :controller => "sessions", :action => "new"
+  post "/login", :controller => "sessions", :action => "create"
   get "/logout", :controller => "sessions", :action => "destroy"
   
   get("/", { :controller => "places", :action => "index" })
