407875d01f7ac4527cbce1d8e99807e83b6b4ab4
Jacob Zastrow <252672008+zastrowj@users.noreply.github.com>
2026-03-06 19:29:12 +0000

    update password input to work with bcrypt

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
     @user = User.new
     @user["username"] = params["username"]
     @user["email"] = params["email"]
-    @user["password"] = params["password"]
+    @user.password = params["password"]
     @user.save
     redirect_to "/"
   end
